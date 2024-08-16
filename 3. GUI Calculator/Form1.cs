5a8e7e51053e958bcd2bc6b33031071ae4ebb1e0
jakubbak-online <jakubbak777@gmail.com>
2024-08-16 12:04:03 +0200

    Added simple GUI Calculator made in WinForms

---
+++ 3. GUI Calculator/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.GUI_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string total;
+        string option;
+        long number1, number2;
+        double result;
+        bool hasCompleted;
+
+        private void NotANumber()
+        {
+            textDisplay.Clear();
+            textDisplay.Text = "NaN";
+            hasCompleted = true;
+        }
+
+        private void FullClear()
+        {
+            textDisplay.Clear();
+            hasCompleted = false;
+            number1 = 0;
+            number2 = 0;
+            result = 0;
+        }
+
+        private void FullClearAfterCalculations()
+        {
+            if (hasCompleted)
+            {
+                FullClear();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            option = "*";
+            number1 = long.Parse(textDisplay.Text);
+
+            textDisplay.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            option = "-";
+            number1 = long.Parse(textDisplay.Text);
+
+            textDisplay.Clear();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button16.Text;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button15.Text;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button14.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button10.Text;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button11.Text;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button12.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button8.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            option = "+";
+            number1 = long.Parse(textDisplay.Text);
+
+            textDisplay.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            option = "/";
+            number1 = long.Parse(textDisplay.Text);
+
+            textDisplay.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FullClearAfterCalculations();
+
+            textDisplay.Text = textDisplay.Text + button5.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textDisplay.Clear();
+        }
+
+        private void textDisplay_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number2 = long.Parse(textDisplay.Text);
+            }catch (System.FormatException){
+                Console.WriteLine(number1);
+                NotANumber();
+                return;
+            }
+
+            switch (option)
+            {
+                case "+":
+                    result = (double)number1 + (double)number2;
+                    break;
+                case "/":
+                    result = (double)number1 / (double)number2;
+                    break;
+                case "-":
+                    result = (double)number1 - (double)number2;
+                    break;
+                case "*":
+                    result = (double)number1 * (double)number2;
+                    break;
+                default:
+                    result = number1;
+                    break;
+            }
+
+            textDisplay.Clear();
+            textDisplay.Text = result.ToString();
+            hasCompleted = true;
+        }
+    }
+}
---
+++ 3. GUI Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("3. GUI Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3. GUI Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("8300e0eb-2f15-40c2-a3a1-1c43b32d009b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
